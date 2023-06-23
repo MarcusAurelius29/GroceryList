5a52ea89cf6f5844c455b74cc27f07ac6ed4929b
MarcusAurelius29 <varungole1999@gmail.com>
2023-06-23 11:52:52 +0530

    second commit

--- main.js
+++ main.js
@@ -4,3 +4,41 @@ function playSong()
     audio.play();
 }
 
+
+var addedItems = [];
+
+function addItem() {
+    var addedItem = document.getElementById("item-input");
+    var input = addedItem.value;
+
+
+    if(input !== "")
+    {
+        var listItem = document.createElement("li");
+        listItem.textContent = input;
+        
+        if(addedItems.includes(input))
+        {
+            alert("You have already added that in your list");
+        }
+        else{
+            addedItems.push(input);
+        }
+        var itemList = document.getElementById("list");
+        
+        itemList.appendChild(listItem);
+        
+
+        addedItem.value = "";
+    }
+}
+
+
+function clearLastItem() {
+    var itemList = document.getElementById("list");
+    var lastItem = itemList.lastElementChild;
+  
+    if (lastItem !== null) {
+      itemList.removeChild(lastItem);
+    }
+  }
